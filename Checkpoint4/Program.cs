e08cb776e9fa591f3894d17210eb4a68593c274b
Johan Nensen <jnensen@hotmail.com>
2018-11-02 14:45:41 +0100

    Add project files.

---
+++ Checkpoint4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkpoint4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
